674df4804971361d497b82faa6d31c60a8dd8c13
Ahan <64554828+AhanR@users.noreply.github.com>
2023-06-04 18:17:42 +0530

    added in fonts, and some basic style

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import { Bree_Serif } from "next/font/google"
 
+const bree = Bree_Serif({
+  weight: '400',
+  subsets : ['latin']
+})
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-orange-500">
+    <main className={"flex min-h-screen flex-col items-center justify-center p-24 bg-orange-500 " + bree.className}  >
       <button
-        className={" border rounded "}
+        className={" rounded-3xl font-bold text-black text-4xl border-black border-[3px] py-4 px-8 " + bree.className}
+        onClick={()=>console.log("hi")}
       >Login</button>
     </main>
   )
